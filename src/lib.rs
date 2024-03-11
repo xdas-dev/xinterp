92e182674600894c4abc1166fad75eb467206d50
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-03-11 17:29:58 +0100

    Add Inverse and DivOp.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+trait Inverse {
+    type Output;
+    fn inverse_exact(
+        self,
+        x0: Self::Output,
+        x1: Self::Output,
+        f0: Self,
+        f1: Self,
+    ) -> Option<Self::Output>;
+    fn inverse_round(self, x0: Self::Output, x1: Self::Output, f0: Self, f1: Self) -> Self::Output;
+    fn inverse_ffill(self, x0: Self::Output, x1: Self::Output, f0: Self, f1: Self) -> Self::Output;
+    fn inverse_bfill(self, x0: Self::Output, x1: Self::Output, f0: Self, f1: Self) -> Self::Output;
+}
+
+impl Inverse for i64 {
+    type Output = u64;
+    fn inverse_exact(self, x0: u64, x1: u64, f0: i64, f1: i64) -> Option<u64> {
+        assert!(f0 < f1, "f1 must greater than f0");
+        assert!(f0 <= self && self <= f1, "f must be in [f0, f1]");
+        f1.checked_sub(f0).expect("f range is to big");
+        let num = (x0 as i128) * ((f1 - self) as i128) + (x1 as i128) * ((self - f0) as i128);
+        let den = (f1 - f0) as i128;
+        num.div_exact(den).map(|x| x as u64)
+    }
+    fn inverse_round(self, x0: u64, x1: u64, f0: i64, f1: i64) -> u64 {
+        assert!(f0 < f1, "f1 must greater than f0");
+        assert!(f0 <= self && self <= f1, "f must be in [f0, f1]");
+        f1.checked_sub(f0).expect("f range is to big");
+        let num = (x0 as i128) * ((f1 - self) as i128) + (x1 as i128) * ((self - f0) as i128);
+        let den = (f1 - f0) as i128;
+        num.div_round(den) as u64
+    }
+    fn inverse_ffill(self, x0: u64, x1: u64, f0: i64, f1: i64) -> u64 {
+        assert!(f0 < f1, "f1 must greater than f0");
+        assert!(f0 <= self && self <= f1, "f must be in [f0, f1]");
+        f1.checked_sub(f0).expect("f range is to big");
+        let num = (x0 as i128) * ((f1 - self) as i128) + (x1 as i128) * ((self - f0) as i128);
+        let den = (f1 - f0) as i128;
+        num.div_ffill(den) as u64
+    }
+    fn inverse_bfill(self, x0: u64, x1: u64, f0: i64, f1: i64) -> u64 {
+        assert!(f0 < f1, "f1 must greater than f0");
+        assert!(f0 <= self && self <= f1, "f must be in [f0, f1]");
+        f1.checked_sub(f0).expect("f range is to big");
+        let num = (x0 as i128) * ((f1 - self) as i128) + (x1 as i128) * ((self - f0) as i128);
+        let den = (f1 - f0) as i128;
+        num.div_bfill(den) as u64
+    }
+}
+
 trait Forward {
     type Output;
     fn forward(self, x0: Self, x1: Self, f0: Self::Output, f1: Self::Output) -> Self::Output;
@@ -10,18 +60,32 @@ impl Forward for u64 {
         assert!(x0 <= self && self <= x1, "x must be in [x0, x1]");
         let num = (f0 as i128) * ((x1 - self) as i128) + (f1 as i128) * ((self - x0) as i128);
         let den = (x1 - x0) as i128;
-        num.rounded_div(den) as i64
+        num.div_round(den) as i64
     }
 }
 
-trait RoundedDiv {
-    fn rounded_div(self, rhs: Self) -> Self;
+trait DivOp {
+    type Output;
+    fn div_exact(self, rhs: Self) -> Option<Self::Output>;
+    fn div_round(self, rhs: Self) -> Self::Output;
+    fn div_ffill(self, rhs: Self) -> Self::Output;
+    fn div_bfill(self, rhs: Self) -> Self::Output;
 }
 
-impl RoundedDiv for u128 {
-    fn rounded_div(self, rhs: u128) -> u128 {
-        let div = self / rhs;
-        let rem = self % rhs;
+impl DivOp for u128 {
+    type Output = u128;
+    fn div_exact(self, rhs: u128) -> Option<u128> {
+        let div = self.div_euclid(rhs);
+        let rem = self.rem_euclid(rhs);
+        if rem == 0 {
+            Some(div)
+        } else {
+            None
+        }
+    }
+    fn div_round(self, rhs: u128) -> u128 {
+        let div = self.div_euclid(rhs);
+        let rem = self.rem_euclid(rhs);
         if rem * 2 < rhs {
             div
         } else if rem * 2 > rhs {
@@ -34,18 +98,136 @@ impl RoundedDiv for u128 {
             }
         }
     }
+    fn div_ffill(self, rhs: u128) -> u128 {
+        self.div_euclid(rhs)
+    }
+    fn div_bfill(self, rhs: u128) -> u128 {
+        let div = self.div_euclid(rhs);
+        let rem = self.rem_euclid(rhs);
+        if rem == 0 {
+            div
+        } else {
+            div + 1
+        }
+    }
 }
 
-impl RoundedDiv for i128 {
-    fn rounded_div(self, rhs: i128) -> i128 {
+impl DivOp for i128 {
+    type Output = i128;
+    fn div_exact(self, rhs: i128) -> Option<i128> {
+        let div = self.div_euclid(rhs);
+        let rem = self.rem_euclid(rhs);
+        if rem == 0 {
+            Some(div)
+        } else {
+            None
+        }
+    }
+    fn div_round(self, rhs: i128) -> i128 {
         let sgn = self.signum() * rhs.signum();
-        sgn * ((self.abs() as u128).rounded_div(rhs.abs() as u128) as i128)
+        sgn * ((self.abs() as u128).div_round(rhs.abs() as u128) as i128)
+    }
+    fn div_ffill(self, rhs: i128) -> i128 {
+        self.div_euclid(rhs)
+    }
+    fn div_bfill(self, rhs: i128) -> i128 {
+        let div = self.div_euclid(rhs);
+        let rem = self.rem_euclid(rhs);
+        if rem == 0 {
+            div
+        } else {
+            div + 1
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_inverse_exact() {
+        assert_eq!(10.inverse_exact(0, 5, 10, 20), Some(0));
+        assert_eq!(11.inverse_exact(0, 5, 10, 20), None);
+        assert_eq!(12.inverse_exact(0, 5, 10, 20), Some(1));
+        assert_eq!(13.inverse_exact(0, 5, 10, 20), None);
+        assert_eq!(20.inverse_exact(0, 5, 10, 20), Some(5));
+
+        assert_eq!((-20).inverse_exact(0, 5, -20, -10), Some(0));
+        assert_eq!((-19).inverse_exact(0, 5, -20, -10), None);
+        assert_eq!((-18).inverse_exact(0, 5, -20, -10), Some(1));
+        assert_eq!((-17).inverse_exact(0, 5, -20, -10), None);
+        assert_eq!((-10).inverse_exact(0, 5, -20, -10), Some(5));
+
+        let x1 = u64::MAX - 3;
+        let f1 = (x1 / 2) as i64;
+        assert_eq!(0.inverse_exact(0, x1, 0, f1), Some(0));
+        assert_eq!(f1.inverse_exact(0, x1, 0, f1), Some(x1));
+        assert_eq!((f1 / 2).inverse_exact(0, x1, 0, f1), Some(x1 / 2));
+    }
+
+    #[test]
+    fn test_inverse_round() {
+        assert_eq!(10.inverse_round(0, 5, 10, 20), 0);
+        assert_eq!(11.inverse_round(0, 5, 10, 20), 0);
+        assert_eq!(12.inverse_round(0, 5, 10, 20), 1);
+        assert_eq!(13.inverse_round(0, 5, 10, 20), 2);
+        assert_eq!(20.inverse_round(0, 5, 10, 20), 5);
+
+        assert_eq!((-20).inverse_round(0, 5, -20, -10), 0);
+        assert_eq!((-19).inverse_round(0, 5, -20, -10), 0);
+        assert_eq!((-18).inverse_round(0, 5, -20, -10), 1);
+        assert_eq!((-17).inverse_round(0, 5, -20, -10), 2);
+        assert_eq!((-10).inverse_round(0, 5, -20, -10), 5);
+
+        let x1 = u64::MAX - 3;
+        let f1 = (x1 / 2) as i64;
+        assert_eq!(0.inverse_round(0, x1, 0, f1), 0);
+        assert_eq!(f1.inverse_round(0, x1, 0, f1), x1);
+        assert_eq!((f1 / 2).inverse_round(0, x1, 0, f1), x1 / 2);
+    }
+
+    #[test]
+    fn test_inverse_ffill() {
+        assert_eq!(10.inverse_ffill(0, 5, 10, 20), 0);
+        assert_eq!(11.inverse_ffill(0, 5, 10, 20), 0);
+        assert_eq!(12.inverse_ffill(0, 5, 10, 20), 1);
+        assert_eq!(13.inverse_ffill(0, 5, 10, 20), 1);
+        assert_eq!(20.inverse_ffill(0, 5, 10, 20), 5);
+
+        assert_eq!((-20).inverse_ffill(0, 5, -20, -10), 0);
+        assert_eq!((-19).inverse_ffill(0, 5, -20, -10), 0);
+        assert_eq!((-18).inverse_ffill(0, 5, -20, -10), 1);
+        assert_eq!((-17).inverse_ffill(0, 5, -20, -10), 1);
+        assert_eq!((-10).inverse_ffill(0, 5, -20, -10), 5);
+
+        let x1 = u64::MAX - 3;
+        let f1 = (x1 / 2) as i64;
+        assert_eq!(0.inverse_ffill(0, x1, 0, f1), 0);
+        assert_eq!(f1.inverse_ffill(0, x1, 0, f1), x1);
+        assert_eq!((f1 / 2).inverse_ffill(0, x1, 0, f1), x1 / 2);
+    }
+
+    #[test]
+    fn test_inverse_bfill() {
+        assert_eq!(10.inverse_bfill(0, 5, 10, 20), 0);
+        assert_eq!(11.inverse_bfill(0, 5, 10, 20), 1);
+        assert_eq!(12.inverse_bfill(0, 5, 10, 20), 1);
+        assert_eq!(13.inverse_bfill(0, 5, 10, 20), 2);
+        assert_eq!(20.inverse_bfill(0, 5, 10, 20), 5);
+
+        assert_eq!((-20).inverse_bfill(0, 5, -20, -10), 0);
+        assert_eq!((-19).inverse_bfill(0, 5, -20, -10), 1);
+        assert_eq!((-18).inverse_bfill(0, 5, -20, -10), 1);
+        assert_eq!((-17).inverse_bfill(0, 5, -20, -10), 2);
+        assert_eq!((-10).inverse_bfill(0, 5, -20, -10), 5);
+
+        let x1 = u64::MAX - 3;
+        let f1 = (x1 / 2) as i64;
+        assert_eq!(0.inverse_bfill(0, x1, 0, f1), 0);
+        assert_eq!(f1.inverse_bfill(0, x1, 0, f1), x1);
+        assert_eq!((f1 / 2).inverse_bfill(0, x1, 0, f1), x1 / 2);
+    }
 
     #[test]
     fn test_forward() {
@@ -59,6 +241,9 @@ mod tests {
         assert_eq!(2.forward(0, 10, -20, -25), -21);
         assert_eq!(3.forward(0, 10, -20, -25), -22);
         assert_eq!(10.forward(0, 10, -20, -25), -25);
+        assert_eq!(0.forward(0, u64::MAX, i64::MIN, i64::MAX), i64::MIN);
+        assert_eq!(u64::MAX.forward(0, u64::MAX, i64::MIN, i64::MAX), i64::MAX);
+        assert_eq!((u64::MAX / 2).forward(0, u64::MAX, i64::MIN, i64::MAX), -1);
     }
 
     #[test]
@@ -74,18 +259,62 @@ mod tests {
     }
 
     #[test]
-    fn test_rounded_div() {
-        assert_eq!(0u128.rounded_div(2), 0);
-        assert_eq!(1u128.rounded_div(2), 0);
-        assert_eq!(2u128.rounded_div(2), 1);
-        assert_eq!(3u128.rounded_div(2), 2);
-        assert_eq!(1u128.rounded_div(3), 0);
-        assert_eq!(2u128.rounded_div(3), 1);
-        assert_eq!(-0i128.rounded_div(2), 0);
-        assert_eq!(-1i128.rounded_div(2), 0);
-        assert_eq!(-2i128.rounded_div(2), -1);
-        assert_eq!(-3i128.rounded_div(2), -2);
-        assert_eq!(-1i128.rounded_div(3), -0);
-        assert_eq!(-2i128.rounded_div(3), -1);
+    fn test_div_exact() {
+        assert_eq!(0u128.div_exact(2), Some(0));
+        assert_eq!(1u128.div_exact(2), None);
+        assert_eq!(2u128.div_exact(2), Some(1));
+        assert_eq!(3u128.div_exact(2), None);
+        assert_eq!(1u128.div_exact(3), None);
+        assert_eq!(2u128.div_exact(3), None);
+        assert_eq!((-1i128).div_exact(2), None);
+        assert_eq!((-2i128).div_exact(2), Some(-1));
+        assert_eq!((-3i128).div_exact(2), None);
+        assert_eq!((-1i128).div_exact(3), None);
+        assert_eq!((-2i128).div_exact(3), None);
+    }
+
+    #[test]
+    fn test_div_round() {
+        assert_eq!(0u128.div_round(2), 0);
+        assert_eq!(1u128.div_round(2), 0);
+        assert_eq!(2u128.div_round(2), 1);
+        assert_eq!(3u128.div_round(2), 2);
+        assert_eq!(1u128.div_round(3), 0);
+        assert_eq!(2u128.div_round(3), 1);
+        assert_eq!((-1i128).div_round(2), 0);
+        assert_eq!((-2i128).div_round(2), -1);
+        assert_eq!((-3i128).div_round(2), -2);
+        assert_eq!((-1i128).div_round(3), 0);
+        assert_eq!((-2i128).div_round(3), -1);
+    }
+
+    #[test]
+    fn test_div_ffill() {
+        assert_eq!(0u128.div_ffill(2), 0);
+        assert_eq!(1u128.div_ffill(2), 0);
+        assert_eq!(2u128.div_ffill(2), 1);
+        assert_eq!(3u128.div_ffill(2), 1);
+        assert_eq!(1u128.div_ffill(3), 0);
+        assert_eq!(2u128.div_ffill(3), 0);
+        assert_eq!((-1i128).div_ffill(2), -1);
+        assert_eq!((-2i128).div_ffill(2), -1);
+        assert_eq!((-3i128).div_ffill(2), -2);
+        assert_eq!((-1i128).div_ffill(3), -1);
+        assert_eq!((-2i128).div_ffill(3), -1);
+    }
+
+    #[test]
+    fn test_div_bfill() {
+        assert_eq!(0u128.div_bfill(2), 0);
+        assert_eq!(1u128.div_bfill(2), 1);
+        assert_eq!(2u128.div_bfill(2), 1);
+        assert_eq!(3u128.div_bfill(2), 2);
+        assert_eq!(1u128.div_bfill(3), 1);
+        assert_eq!(2u128.div_bfill(3), 1);
+        assert_eq!((-1i128).div_bfill(2), 0);
+        assert_eq!((-2i128).div_bfill(2), -1);
+        assert_eq!((-3i128).div_bfill(2), -1);
+        assert_eq!((-1i128).div_bfill(3), 0);
+        assert_eq!((-2i128).div_bfill(3), 0);
     }
 }
