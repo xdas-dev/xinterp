aa92ac8670f4a309dd403d1fb045c7a289f1c656
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-03-14 11:18:34 +0100

    Add F64 scheme.

--- src/schemes.rs
+++ src/schemes.rs
@@ -1,4 +1,5 @@
 use crate::divop::DivOp;
+use std::cmp::Ordering;
 
 pub trait Forward<F>: Copy + Ord {
     fn forward(self, x0: Self, x1: Self, f0: F, f1: F) -> F;
@@ -19,10 +20,11 @@ impl Forward<i64> for u64 {
     }
 }
 
-impl Forward<f64> for u64 {
-    fn forward(self, x0: u64, x1: u64, f0: f64, f1: f64) -> f64 {
+impl Forward<F64> for u64 {
+    fn forward(self, x0: u64, x1: u64, f0: F64, f1: F64) -> F64 {
         let t = ((self - x0) as f64) / ((x1 - x0) as f64);
-        (1.0 - t) * f0 + t * f1
+        let value = (1.0 - t) * f0.value + t * f1.value;
+        F64 { value }
     }
 }
 
@@ -75,22 +77,38 @@ impl Inverse<u64> for i64 {
     }
 }
 
-impl Inverse<u64> for f64 {
-    fn inverse_exact(self, x0: u64, x1: u64, f0: f64, f1: f64) -> Option<u64> {
-        let t = (self - f0) / (f1 - f0);
-        x0 + t * (x1 - x0) * t
+impl Inverse<u64> for F64 {
+    fn inverse_exact(self, x0: u64, x1: u64, f0: F64, f1: F64) -> Option<u64> {
+        let t = (self.value - f0.value) / (f1.value - f0.value);
+        let den = 2u64.pow(53);
+        let num = (t * (den as f64)) as u64;
+        let num = (x0 as u128) * (num as u128) + (x1 as u128) * ((den - num) as u128);
+        let den = den as u128;
+        num.div_exact(den).map(|x| x as u64)
     }
-    fn inverse_round(self, x0: u64, x1: u64, f0: f64, f1: f64) -> u64 {
-        let t = (self - f0) / (f1 - f0);
-        x0 + t * (x1 - x0) * t
+    fn inverse_round(self, x0: u64, x1: u64, f0: F64, f1: F64) -> u64 {
+        let t = (self.value - f0.value) / (f1.value - f0.value);
+        let den = 2u64.pow(53);
+        let num = (t * (den as f64)) as u64;
+        let num = (x0 as u128) * (num as u128) + (x1 as u128) * ((den - num) as u128);
+        let den = den as u128;
+        num.div_round(den) as u64
     }
-    fn inverse_ffill(self, x0: u64, x1: u64, f0: f64, f1: f64) -> u64 {
-        let t = (self - f0) / (f1 - f0);
-        x0 + t * (x1 - x0) * t
+    fn inverse_ffill(self, x0: u64, x1: u64, f0: F64, f1: F64) -> u64 {
+        let t = (self.value - f0.value) / (f1.value - f0.value);
+        let den = 2u64.pow(53);
+        let num = (t * (den as f64)) as u64;
+        let num = (x0 as u128) * (num as u128) + (x1 as u128) * ((den - num) as u128);
+        let den = den as u128;
+        num.div_ffill(den) as u64
     }
-    fn inverse_bfill(self, x0: u64, x1: u64, f0: f64, f1: f64) -> u64 {
-        let t = (self - f0) / (f1 - f0);
-        x0 + t * (x1 - x0) * t
+    fn inverse_bfill(self, x0: u64, x1: u64, f0: F64, f1: F64) -> u64 {
+        let t = (self.value - f0.value) / (f1.value - f0.value);
+        let den = 2u64.pow(53);
+        let num = (t * (den as f64)) as u64;
+        let num = (x0 as u128) * (num as u128) + (x1 as u128) * ((den - num) as u128);
+        let den = den as u128;
+        num.div_bfill(den) as u64
     }
 }
 
@@ -111,5 +129,28 @@ pub trait ToSigned<S> {
 impl ToSigned<i64> for u64 {
     fn to_signed(self) -> i64 {
         self.wrapping_add(i64::MIN as u64) as i64
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+struct F64 {
+    value: f64,
+}
+
+impl F64 {
+    fn new(value: f64) -> Option<F64> {
+        if value.is_nan() {
+            None
+        } else {
+            Some(F64 { value })
+        }
+    }
+}
+
+impl Eq for F64 {}
+
+impl Ord for F64 {
+    fn cmp(&self, other: &F64) -> Ordering {
+        self.partial_cmp(other).unwrap()
     }
 }
