475ffebabb0e80ffacfaee37b310b7d4456882ac
Alister Trabattoni <alister.trabattoni@gmail.com>
2024-03-15 22:16:34 +0100

    Add Method and implements it for u128.

--- src/divop.rs
+++ src/divop.rs
@@ -1,7 +1,16 @@
 //! Integer division with different rounding rules
 
+pub enum Method {
+    None,
+    Nearest,
+    ForwardFill,
+    BackwardFill,
+}
+
 /// Traits for performing division operations with different rounding rules.
 pub trait DivOp: Sized {
+    fn div(self, rhs: Self, method: Method) -> Option<Self>;
+
     /// Performs division and returns the quotien if the remainder is zero,
     /// otherwise returns `None`.
     fn div_exact(self, rhs: Self) -> Option<Self>;
@@ -18,6 +27,40 @@ pub trait DivOp: Sized {
 }
 
 impl DivOp for u128 {
+    fn div(self, rhs: u128, method: Method) -> Option<u128> {
+        let div = self.div_euclid(rhs);
+        let rem = self.rem_euclid(rhs);
+        match method {
+            Method::None => {
+                if rem == 0 {
+                    Some(div)
+                } else {
+                    None
+                }
+            }
+            Method::Nearest => {
+                if rem * 2 < rhs {
+                    Some(div)
+                } else if rem * 2 > rhs {
+                    Some(div + 1)
+                } else {
+                    if div % 2 == 0 {
+                        Some(div)
+                    } else {
+                        Some(div + 1)
+                    }
+                }
+            }
+            Method::ForwardFill => Some(div),
+            Method::BackwardFill => {
+                if rem == 0 {
+                    Some(div)
+                } else {
+                    Some(div + 1)
+                }
+            }
+        }
+    }
     fn div_exact(self, rhs: u128) -> Option<u128> {
         let div = self.div_euclid(rhs);
         let rem = self.rem_euclid(rhs);
@@ -57,6 +100,9 @@ impl DivOp for u128 {
 }
 
 impl DivOp for i128 {
+    fn div(self, rhs: i128, method: Method) -> Option<i128> {
+        todo!();
+    }
     fn div_exact(self, rhs: i128) -> Option<i128> {
         let div = self.div_euclid(rhs);
         let rem = self.rem_euclid(rhs);
@@ -90,12 +136,12 @@ mod tests {
 
     #[test]
     fn test_div_exact() {
-        assert_eq!(0u128.div_exact(2), Some(0));
-        assert_eq!(1u128.div_exact(2), None);
-        assert_eq!(2u128.div_exact(2), Some(1));
-        assert_eq!(3u128.div_exact(2), None);
-        assert_eq!(1u128.div_exact(3), None);
-        assert_eq!(2u128.div_exact(3), None);
+        assert_eq!(0u128.div(2, Method::None), Some(0));
+        assert_eq!(1u128.div(2, Method::None), None);
+        assert_eq!(2u128.div(2, Method::None), Some(1));
+        assert_eq!(3u128.div(2, Method::None), None);
+        assert_eq!(1u128.div(3, Method::None), None);
+        assert_eq!(2u128.div(3, Method::None), None);
         assert_eq!((-1i128).div_exact(2), None);
         assert_eq!((-2i128).div_exact(2), Some(-1));
         assert_eq!((-3i128).div_exact(2), None);
@@ -105,12 +151,12 @@ mod tests {
 
     #[test]
     fn test_div_round() {
-        assert_eq!(0u128.div_round(2), 0);
-        assert_eq!(1u128.div_round(2), 0);
-        assert_eq!(2u128.div_round(2), 1);
-        assert_eq!(3u128.div_round(2), 2);
-        assert_eq!(1u128.div_round(3), 0);
-        assert_eq!(2u128.div_round(3), 1);
+        assert_eq!(0u128.div(2, Method::Nearest), Some(0));
+        assert_eq!(1u128.div(2, Method::Nearest), Some(0));
+        assert_eq!(2u128.div(2, Method::Nearest), Some(1));
+        assert_eq!(3u128.div(2, Method::Nearest), Some(2));
+        assert_eq!(1u128.div(3, Method::Nearest), Some(0));
+        assert_eq!(2u128.div(3, Method::Nearest), Some(1));
         assert_eq!((-1i128).div_round(2), 0);
         assert_eq!((-2i128).div_round(2), -1);
         assert_eq!((-3i128).div_round(2), -2);
@@ -120,12 +166,12 @@ mod tests {
 
     #[test]
     fn test_div_ffill() {
-        assert_eq!(0u128.div_ffill(2), 0);
-        assert_eq!(1u128.div_ffill(2), 0);
-        assert_eq!(2u128.div_ffill(2), 1);
-        assert_eq!(3u128.div_ffill(2), 1);
-        assert_eq!(1u128.div_ffill(3), 0);
-        assert_eq!(2u128.div_ffill(3), 0);
+        assert_eq!(0u128.div(2, Method::ForwardFill), Some(0));
+        assert_eq!(1u128.div(2, Method::ForwardFill), Some(0));
+        assert_eq!(2u128.div(2, Method::ForwardFill), Some(1));
+        assert_eq!(3u128.div(2, Method::ForwardFill), Some(1));
+        assert_eq!(1u128.div(3, Method::ForwardFill), Some(0));
+        assert_eq!(2u128.div(3, Method::ForwardFill), Some(0));
         assert_eq!((-1i128).div_ffill(2), -1);
         assert_eq!((-2i128).div_ffill(2), -1);
         assert_eq!((-3i128).div_ffill(2), -2);
@@ -135,12 +181,12 @@ mod tests {
 
     #[test]
     fn test_div_bfill() {
-        assert_eq!(0u128.div_bfill(2), 0);
-        assert_eq!(1u128.div_bfill(2), 1);
-        assert_eq!(2u128.div_bfill(2), 1);
-        assert_eq!(3u128.div_bfill(2), 2);
-        assert_eq!(1u128.div_bfill(3), 1);
-        assert_eq!(2u128.div_bfill(3), 1);
+        assert_eq!(0u128.div(2, Method::BackwardFill), Some(0));
+        assert_eq!(1u128.div(2, Method::BackwardFill), Some(1));
+        assert_eq!(2u128.div(2, Method::BackwardFill), Some(1));
+        assert_eq!(3u128.div(2, Method::BackwardFill), Some(2));
+        assert_eq!(1u128.div(3, Method::BackwardFill), Some(1));
+        assert_eq!(2u128.div(3, Method::BackwardFill), Some(1));
         assert_eq!((-1i128).div_bfill(2), 0);
         assert_eq!((-2i128).div_bfill(2), -1);
         assert_eq!((-3i128).div_bfill(2), -1);
